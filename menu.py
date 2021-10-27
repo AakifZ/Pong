1935c8a9214cd51d70619bc6af7b9aa698e29ace
UwemEkong <uweme6@gmail.com>
2021-10-26 23:10:02 -0500

    rewrite gamemode

--- menu.py
+++ menu.py
@@ -3,11 +3,10 @@ import pygame_menu
 from pygame_menu.baseimage import BaseImage
 import pygame_menu.font
 from PONG import init_game
-from Gamemode import Gamemode
+from Gamemode import create_gamemode_window
 
 pygame.init()
 surface = pygame.display.set_mode((1080, 720))
-game = Gamemode(True, 1080, 720, 'singleplayer', 'easy')
 font = pygame_menu.font.FONT_NEVIS
 class menu():
 
@@ -26,7 +25,7 @@ class menu():
         mainmenu = pygame_menu.Menu("Welcome to PONG!", 1080, 720,theme=mytheme)
         mainmenu.add.text_input("Enter name: ", default= "User")
         mainmenu.add.button('Play',  init_game)
-        mainmenu.add.button('Game Options', game.create_gamemode_window)
+        mainmenu.add.button('Game Options', create_gamemode_window)
         mainmenu.add.button('Settings')
         mainmenu.add.button("Leaderboard")
         mainmenu.add.button("Quit", pygame_menu.events.EXIT)

ee2913fc6fbbab98ddc5692cb8344e9b30a3bcb2
AdamChaplin23 <b2482993@ben.edu>
2021-10-29 12:07:44 -0500

    db creation script and population script

---
+++ MySQL/leaderboardpop.sql
@@ -0,0 +1,4 @@
+INSERT into mydb.pongLeaderboard(idNUmber, userName, numberOfWins) VALUE(1, 'Uwem', 2);
+INSERT into mydb.pongLeaderboard(idNUmber, userName, numberOfWins) VALUE(2, 'Adam', 4);
+INSERT into mydb.pongLeaderboard(idNUmber, userName, numberOfWins) VALUE(3, 'Aakif', 3);
+INSERT into mydb.pongLeaderboard(idNUmber, userName, numberOfWins) VALUE(4, 'Shuaib', 2);
